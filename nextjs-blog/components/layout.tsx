f8d8c603fb452a0f05d53d8543a3ffc385f72c19
Serkan YESILDAG <syesildag@hotmail.com>
2021-04-11 19:45:52 +0200

    layout name

--- nextjs-blog/components/layout.tsx
+++ nextjs-blog/components/layout.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import utilStyles from '../styles/utils.module.css';
 import styles from './layout.module.css';
 
-const name = '[Your Name]';
+const name = 'Serkan YESILDAG';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }: { children: React.ReactNode, home?: boolean }) {
