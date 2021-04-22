f6931308f8563d19b229ec5933cc7122979d8877
Serkan YESILDAG <syesildag@hotmail.com>
2021-04-22 20:41:16 +0200

    worker queue base options

--- lib/src/tasks/workers.ts
+++ lib/src/tasks/workers.ts
@@ -3,21 +3,24 @@ import { Worker } from 'bullmq';
 import env from '../utils/env';
 import logger from '../utils/logger';
 import { factory, JobDataType } from './jobs';
-import { JobName, name } from './queue';
+import { JobName, name, queueBaseOptions } from './queue';
 
 const workers: Worker<JobDataType, void, JobName>[] = [];
 
 for (let index = 0; index < env.NB_TASK_WORKERS; index++) {
    workers.push(
-      new Worker<JobDataType, void, JobName>(name, async job => {
-         const processor = factory.create(job.data);
-         if (!processor) {
-            logger.debug(`no processor for job ${JSON.stringify(job)}`);
-            return;
-         }
-         logger.info(`processing job ${JSON.stringify(job)}`);
-         return await processor.process(job);
-      })
+      new Worker<JobDataType, void, JobName>(
+         name,
+         async job => {
+            const processor = factory.create(job.data);
+            if (!processor) {
+               logger.debug(`no processor for job ${JSON.stringify(job)}`);
+               return;
+            }
+            logger.info(`processing job ${JSON.stringify(job)}`);
+            return await processor.process(job);
+         },
+         queueBaseOptions)
    );
 }
 
