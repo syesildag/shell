ab80a12e64185c1dd39902e8302085390dee7451
Serkan YESILDAG <syesildag@hotmail.com>
2021-04-25 20:27:47 +0200

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,9 @@ import Link from 'next/link';
 import React from 'react';
 
 import Layout, { siteTitle } from '../components/layout';
-import { PostData } from '../lib/src/posts';
 import utilStyles from '../styles/utils.module.css';
 
 export interface HomeProps {
-  allPostsData: Array<PostData>
 }
 
 export default class Home extends React.Component<HomeProps> {
@@ -17,7 +15,6 @@ export default class Home extends React.Component<HomeProps> {
   }
 
   render() {
-    const { allPostsData } = this.props;
     return (
       <Layout home>
         <Head>
